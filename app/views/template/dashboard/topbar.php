b738ea8db5c20a911aa51addf65d7c898bbb57f3
riyandarmawan <riyandarmawan042@gmail.com>
2024-06-04 08:20:50 +0700

    fixed the topbar

--- app/views/template/dashboard/topbar.php
+++ app/views/template/dashboard/topbar.php
@@ -12,7 +12,8 @@
             <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
             <div class="input-group-append">
                 <button class="btn btn-primary" type="button">
-                    <i class="fas fa-search fa-sm"></i>
+                    <!-- <i class="fas fa-search fa-sm"></i> -->
+                    Cari
                 </button>
             </div>
         </div>
@@ -28,15 +29,15 @@
             </a>
         </li>
 
-        <div class="topbar-divider d-none d-sm-block"></div>
+        <!-- <div class="topbar-divider d-none d-sm-block"></div> -->
 
         <!-- Nav Item - User Information -->
-        <li class="nav-item dropdown no-arrow">
+        <!-- <li class="nav-item dropdown no-arrow">
             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <span class="mr-2 d-none d-lg-inline text-gray-600 small">Douglas McGee</span>
                 <img class="img-profile rounded-circle" src="public/img/undraw_profile.svg">
             </a>
-            <!-- Dropdown - User Information -->
+            Dropdown - User Information
             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
                 <a class="dropdown-item" href="#">
                     <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
@@ -56,7 +57,7 @@
                     Logout
                 </a>
             </div>
-        </li>
+        </li> -->
 
     </ul>
 
